06fd488d2dd380c8950051c6d3fc5c941637e1a3
Jay Syslo <jay.sureau@gmail.com>
2026-09-06 22:10:18 -0400

    Fixed Issue #550

--- src/makintikz/_axes.py
+++ src/makintikz/_axes.py
@@ -152,6 +152,7 @@ class MyAxes:
             self._subplot()
 
         data.current_axis_options = set()  # Reset axis options
+        data.rectangle_legends = set()     # Reset per-axis legend dedup tracking
 
         self._set_hide_axis()
         self._set_plot_title()
@@ -179,6 +180,7 @@ class MyAxes:
             return
 
         self.data.current_axis_options = set()
+        self.data.rectangle_legends = set()     # Reset per-axis legend dedup tracking
 
         xlim, ylim = self._set_axis_limits()
 
